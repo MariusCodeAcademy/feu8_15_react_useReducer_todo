f502f15911bb31ff97c093c473cd41a4f563d02a
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-25 14:09:27 +0300

    todo up

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,10 +1,16 @@
+import { useReducer, useState } from 'react';
+
 const initTodos = [
-  { id: 1, value: 'Buy Eggs', done: false, date: '' },
-  { id: 2, value: 'Go to Shopping', done: true, date: '' },
-  { id: 3, value: 'Do a 100 pushups', done: false, date: '' },
+  { id: 1, title: 'Buy Eggs', done: false, date: '' },
+  { id: 2, title: 'Go to Shopping', done: true, date: '' },
+  { id: 3, title: 'Do a 100 pushups', done: false, date: '' },
 ];
 
+function todoReducer(state, action) {}
+
 function Todo() {
+  const [newTodoVal, setNewTodoVal] = useState('');
+  const [state, dispatch] = useReducer(todoReducer, initTodos);
   // sukirti useReducer() fn
   // atvaizduoti sarasa is state
   // atvaizdtuoti todo kurie yra baigti perbrauktus ir pilkus (prideti klase)
@@ -13,10 +19,12 @@ function Todo() {
   // padaryti kad eitu istrinti todo
   // padaryti kad paspaudus ant title todo taptu baigtas (done: false|true)
   // suskaidyti i atskirus komponentus
+
+  console.log('state ===', state);
   return (
     <div>
       <h2>Welcome to our Todo</h2>
-
+      <p>{state.map(({ title }) => title).join(', ')}</p>
       <form>
         <fieldset>
           <legend>Add todo</legend>
